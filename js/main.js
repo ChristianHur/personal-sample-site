3db06be3feec3738fadcc16142a8aea75b32340d
Christian Hur <chrisvhur@gmail.com>
2022-12-07 19:24:14 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 
-function show() {
-    var video = document.getElementById("video");
-    var audio = document.getElementById("audio");
+const show = () => {
+    const video = document.getElementById("video");
+    const audio = document.getElementById("audio");
     video.style.display = "block";
     audio.style.display = "block";
 }
 
-
-function hide() {
-    var video = document.getElementById("video");
-    var audio = document.getElementById("audio");
+const hide = () => {
+    const video = document.getElementById("video");
+    const audio = document.getElementById("audio");
     video.style.display = "none";
     audio.style.display = "none";
 }
+
 //
-// var rec1 = [101,"Kevin Smith","JavaScript 2","A-","Winter","2019"];
-// var rec2 = [102,"Lucy Lane","JavaScript 2","A","Winter","2019"];
+// const rec1 = [101,"Kevin Smith","JavaScript 2","A-","Winter","2019"];
+// const rec2 = [102,"Lucy Lane","JavaScript 2","A","Winter","2019"];
 //
 // function return2DArray(rec1, rec2) {
 //
